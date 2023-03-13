c901f83c91e053171c2b061c3ce7fa51399f12ff
Uchechukwu Daniel <danteuche@gmail.com>
2023-03-13 07:44:37 -0700

    alx

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
-
-// Script that prints a message based on var passed to it
-
 const count = process.argv.length;
 console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
