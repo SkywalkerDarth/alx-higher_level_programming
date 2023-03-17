922b73c1cd8ac1a934f09ad1ee4a2c3933a17dea
SkywalkerDarth <danteuche@gmail.com>
2023-03-17 11:21:01 +0000

    cool

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -16,11 +16,22 @@ if __name__ == '__main__':
     """
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY '{}' \
-                 ORDER BY states.id ASC".format(argv[4])))
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                *
+            FROM
+                states
+            WHERE
+                name LIKE BINARY %(name)s
+            ORDER BY
+                states.id ASC
+        """, {
+            'name': argv[4]
+        })
+
     rows = cur.fetchall()
 
-    for row in rows:
-        print(row)
+    if rows is not None:
+	for row in rows:
+            print(row)
