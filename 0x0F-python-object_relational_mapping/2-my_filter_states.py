9478d21fc57a6efe486a31886630207f9f5c4989
SkywalkerDarth <danteuche@gmail.com>
2023-03-17 11:05:44 +0000

    4-argv

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-This script lists all states with
-a `name` starting with the letter `N`
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
 from the database `hbtn_0e_0_usa`.
 """
 
@@ -17,8 +18,8 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY 'N%' \
-                 ORDER BY states.id ASC")
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(argv[4])))
     rows = cur.fetchall()
 
     for row in rows:
