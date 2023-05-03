5dc820efe82539492b497980b416c54fc509783f
SkywalkerDarth <danteuche@gmail.com>
2023-05-03 11:31:32 -0700

    all tasks

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -5,6 +5,6 @@ const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
 $.get(url, function (data, textStatus) {
   const res = data.results;
   for (let i = 0; i < res.lenght; i++) {
-    $('ul#list_movies').append('<li>' + res[i].title + '</li>');
+    $('UL#list_movies').append('<li>' + res[i].title + '</li>');
   }
 });
